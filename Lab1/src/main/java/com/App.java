2cef79e1e9f6b1c64833962766919e5944cbfe77
nataliaK <nataliakiselyk@gmail.com>
2018-10-07 14:19:06 +0300

    Fixes according to check stype plugin

--- Lab1/src/main/java/com/App.java
+++ Lab1/src/main/java/com/App.java
@@ -5,58 +5,67 @@ import java.util.Collections;
 import java.util.Scanner;
 
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a startpoint of the interval (int): ");
         int startPoint = sc.nextInt();
         System.out.println("Enter an endpoint of the interval (int): ");
         int endPoint = sc.nextInt();
         if (startPoint < endPoint) {
-			System.out.println("Odd numbers from [" + startPoint + "; " + endPoint + "] interval");
-			printOddNumbersFromInterval(startPoint, endPoint);
-			System.out.println();
-			System.out.println("Even numbers from [" + startPoint + "; " + endPoint + "] interval");
+            System.out.println("Odd numbers from [" + startPoint + "; "
+                    + endPoint + "] interval");
+            printOddNumbersFromInterval(startPoint, endPoint);
+            System.out.println();
+			System.out.println("Even numbers from [" + startPoint
+                    + "; " + endPoint + "] interval");
 			printEvenNumbers(startPoint, endPoint);
-			long sumOfOddNumbers = oddNumbersSum(startPoint, endPoint);
-			long sumOfEvenNumbers = evenNumbersSum(startPoint, endPoint);
+			long sumOfOddNumbers = oddNumbersSum(startPoint,
+                    endPoint);
+			long sumOfEvenNumbers = evenNumbersSum(startPoint,
+                    endPoint);
 			System.out.println();
-			System.out.println("Sum of odd numbers = " + sumOfOddNumbers);
-			System.out.println("Sum of even numbers = " + sumOfEvenNumbers);
-			System.out.println("Sum of even and odd numbers = " + oddAndEvenNumbersSum(sumOfOddNumbers, sumOfEvenNumbers));
-
+			System.out.println("Sum of odd numbers = "
+                    + sumOfOddNumbers);
+			System.out.println("Sum of even numbers = "
+                    + sumOfEvenNumbers);
+			System.out.println("Sum of even and odd numbers = "
+                    + oddAndEvenNumbersSum(sumOfOddNumbers, sumOfEvenNumbers));
             int theBiggestOddNumber = theBiggestOddNumber(startPoint, endPoint);
-            int theBiggestEvenNumber = theBiggestEvenNumber(startPoint, endPoint);
+            int theBiggestEvenNumber = theBiggestEvenNumber(startPoint,
+                    endPoint);
             System.out.println("Enter a size of Fibonacci number: ");
             int fibonacciNumberSize = sc.nextInt();
             if (fibonacciNumberSize == 0) {
                 System.out.println(1);
             } else {
-                ArrayList<Long> theFibonacciNumbersList = fibonacciNumbers(fibonacciNumberSize, theBiggestOddNumber, theBiggestEvenNumber);
+                ArrayList<Long> theFibonacciNumbersList = fibonacciNumbers(
+                        fibonacciNumberSize, theBiggestOddNumber,
+                        theBiggestEvenNumber);
                 System.out.println("Fibonacci numbers: ");
                 for (int i = 0; i < theFibonacciNumbersList.size(); i++) {
                     System.out.println(theFibonacciNumbersList.get(i));
                 }
                 System.out.println();
-                double thePesentageOfOddNumbers = showPersantageOfOddNumbers(fibonacciNumberSize, theFibonacciNumbersList);
-                System.out.println("The persentage of odd Fibonacci numbers: " + thePesentageOfOddNumbers);
-                System.out.println("The persentage of even Fibonacci numbers: " + (100 - thePesentageOfOddNumbers));
+                double thePesentageOfOddNumbers = showPersantageOfOddNumbers(
+                        fibonacciNumberSize, theFibonacciNumbersList);
+                System.out.println("The persentage of odd Fibonacci numbers: "
+                        + thePesentageOfOddNumbers);
+                System.out.println("The persentage of even Fibonacci numbers: "
+                        + (100 - thePesentageOfOddNumbers));
             }
-
-
         } else {
             System.out.println("End point can't be lower than a start point");
         }
 
     }
 
-    private static boolean isEven (long aNumber) {
+    private static boolean isEven(final long aNumber) {
         return ((aNumber % 2) == 0); // (aNumber & 1) == 0)
     }
 
-    public static void printOddNumbersFromInterval(int startPoint, int endPoint) {
+    public static void printOddNumbersFromInterval(final int startPoint,
+                                                   final int endPoint) {
         for (int i = startPoint; i <= endPoint; i++) {
             if (!isEven(i)) {
                 System.out.println(i);
@@ -64,7 +73,8 @@ public class App
         }
     }
 
-    public static void printEvenNumbers(int startPoint, int endPoint) {
+    public static void printEvenNumbers(final int startPoint,
+                                        final int endPoint) {
         for (int j = endPoint; j >= startPoint; j--) {
             if (isEven(j)) {
                 System.out.println(j);
@@ -72,7 +82,8 @@ public class App
         }
     }
 
-    public static long oddNumbersSum(int startPoint, int endPoint) {
+    public static long oddNumbersSum(final int startPoint,
+                                     final int endPoint) {
         long theSum = 0;
         for (int i = startPoint; i <= endPoint; i++) {
             if (!isEven(i)) {
@@ -83,7 +94,8 @@ public class App
     }
 
 
-    public static long evenNumbersSum(int startPoint, int endPoint) {
+    public static long evenNumbersSum(final int startPoint,
+                                      final int endPoint) {
         long theSum = 0;
         for (int i = startPoint; i <= endPoint; i++) {
             if (isEven(i)) {
@@ -94,12 +106,14 @@ public class App
     }
 
 
-    public static long oddAndEvenNumbersSum(long oddNumbersSum, long evenNumbersSum) {
+    public static long oddAndEvenNumbersSum(final long oddNumbersSum,
+                                            final long evenNumbersSum) {
         return oddNumbersSum + evenNumbersSum;
     }
 
 
-    public static int theBiggestOddNumber(int startPoint, int endPoint) {
+    public static int theBiggestOddNumber(final int startPoint,
+                                          final int endPoint) {
         ArrayList<Integer> oddNumbersList = new ArrayList();
         for (int i = startPoint; i <= endPoint; i++) {
             if (!isEven(i)) {
@@ -107,11 +121,12 @@ public class App
             }
         }
         Collections.sort(oddNumbersList);
-        return oddNumbersList.get(oddNumbersList.size()-1);
+        return oddNumbersList.get(oddNumbersList.size() - 1);
     }
 
 
-    public static int theBiggestEvenNumber(int startPoint, int endPoint) {
+    public static int theBiggestEvenNumber(final int startPoint,
+                                           final int endPoint) {
         ArrayList<Integer> evenNumbersList = new ArrayList<Integer>();
         for (int i = startPoint; i <= endPoint; i++) {
             if (isEven(i)) {
@@ -119,11 +134,12 @@ public class App
             }
         }
         Collections.sort(evenNumbersList);
-        return evenNumbersList.get(evenNumbersList.size()-1);
+        return evenNumbersList.get(evenNumbersList.size() - 1);
     }
 
 
-    public static ArrayList<Long> fibonacciNumbers(int n, long theBiggestOddNumber, long theBiggestEvenNumber) {
+    public static ArrayList<Long> fibonacciNumbers(
+            final int n, long theBiggestOddNumber, long theBiggestEvenNumber) {
         long temp = 0;
         ArrayList<Long> fibonacciNumbersList = new ArrayList<Long>();
         for (int i = 2; i <= n; i++) {
@@ -136,17 +152,15 @@ public class App
     }
 
 
-    public static double showPersantageOfOddNumbers(int n, ArrayList<Long> fibonacciNumbersList) {
+    public static double showPersantageOfOddNumbers(
+            final int n, final ArrayList<Long> fibonacciNumbersList) {
         ArrayList<Long> oddNumbersList = new ArrayList<Long>();
-        for (int i = 2; i < fibonacciNumbersList.size(); i++) {
+        for (int i = 0; i < fibonacciNumbersList.size(); i++) {
             if (isEven(fibonacciNumbersList.get(i))) {
                 oddNumbersList.add(fibonacciNumbersList.get(i));
             }
         }
-        double thePersantage = (oddNumbersList.size() * 100) / n;
-        return thePersantage;
+        return (oddNumbersList.size() * 100.0) / n;
     }
-
-
 }
 
